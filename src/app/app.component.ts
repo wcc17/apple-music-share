2680a5b9ca728bc4a460576fae7aab8c23866217
Christian Curry <christian.curry17@gmail.com>
2018-12-15 11:04:09 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MusicKitService } from './services/music-kit.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apple-music-share';
+  
+  title = 'Apple Music Share';
+
+  //reserve the constructor for simple initilization
+  constructor(private musicKitService: MusicKitService) { }
+
+  //use this for real stuff 
+  ngOnInit() {
+    this.musicKitService.testMethod();
+  }
 }
---
+++ src/app/components/music-player/music-player.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-music-player',
+  templateUrl: './music-player.component.html',
+  styleUrls: ['./music-player.component.css']
+})
+export class MusicPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  playMusic(): void {
+
+  }
+
+}
