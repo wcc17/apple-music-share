7bd25a1609af7a4ebb0358417b19fc4f1386f0cf
Christian Curry <christian.curry17@gmail.com>
2018-12-16 17:23:38 -0500

    set up a basic page layout

--- src/app/services/music-kit.service.ts
+++ src/app/services/music-kit.service.ts
@@ -11,28 +11,28 @@ export class MusicKitService {
 
   constructor() { 
     MusicKit.configure({
-      developerToken: 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkFKUFoyU1E3U0YifQ.eyJpc3MiOiI0U1JFNTlGVzk0IiwiaWF0IjoxNTQ0ODQ3MzA0LCJleHAiOjE1NDQ4OTA1MDR9.abonxZpEaosHOxym2D01kiNMVikwmlRWu_XluGwZ6rZm1t2muEaFtpgw047vrMYp5uOk0AcYgCF2J0J7TvNt7Q',
+      developerToken: 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkFKUFoyU1E3U0YifQ.eyJpc3MiOiI0U1JFNTlGVzk0IiwiaWF0IjoxNTQ0OTc3MTU0LCJleHAiOjE1NDU1ODE5NTR9.HXKv30tNE9GoCD5QmVHu9WCdrGOHp1zJohziWugfkixTmEBfsa745CM-3Ovq6eVLSvmFNX-lvIs8hQ4ZGfLwhA',
       app: { 
         name: 'Apple Music Share',
         build: '1.0'
       }
     });
 
-    //ensure user is authorized
-    this.musicKit = MusicKit.getInstance();
-    this.musicKit.authorize().then( () => {
-      this.isAuthorized = true;
-    });
+    // //ensure user is authorized
+    // this.musicKit = MusicKit.getInstance();
+    // this.musicKit.authorize().then( () => {
+    //   this.isAuthorized = true;
+    // });
   }
 
   testMethod(): void {
     console.log('calling the test method');
 
-    this.musicKit.setQueue({
-      album: '1025210938'
-    }).then( () => {
-      console.log('going to play the album');
-      this.musicKit.play();
-    });
+    // this.musicKit.setQueue({
+    //   album: '1025210938'
+    // }).then( () => {
+    //   console.log('going to play the album');
+    //   this.musicKit.play();
+    // });
   }
 }
