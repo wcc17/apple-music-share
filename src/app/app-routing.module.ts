0ac5371c5cd777a65aec6a520b48dd7d296975c8
Christian Curry <christian.curry17@gmail.com>
2018-12-17 09:06:42 -0500

    Began to add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QueueComponent } from './components/queue/queue.component';
+import { RecentlyAddedComponent } from './components/recently-added/recently-added.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'queue', component: QueueComponent },
+  { path: 'recently-added', component: RecentlyAddedComponent },
+  { path: '**', redirectTo: '/queue', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
