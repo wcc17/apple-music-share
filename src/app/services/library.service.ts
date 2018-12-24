089427fe0c239f30979d22fe9c4c33d11b908d1c
Christian Curry <christian.curry17@gmail.com>
2018-12-24 15:39:57 -0500

    added Apple's artwork URL formatting method and added an event listener for when media changes

--- src/app/services/library.service.ts
+++ src/app/services/library.service.ts
@@ -15,13 +15,6 @@ export class LibraryService {
     return from(this.library.songs( null , { limit: this.songRequestLimit, offset: startIndex }));
   }
 
-  getValidArtworkUrl(artworkUrl: string, width: number, height: number): string {
-    artworkUrl = artworkUrl.replace('{w}', width.toString());
-    artworkUrl = artworkUrl.replace('{h}', height.toString());
-
-    return artworkUrl;
-  }
-
   getSongRequestLimit(): number {
     return this.songRequestLimit;
   }
