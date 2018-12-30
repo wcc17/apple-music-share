b326fc9a46dadc26c7f98ddb6e507c89413a4432
Christian Curry <christian.curry17@gmail.com>
2018-12-30 12:30:50 -0500

    added content to the recently added page

--- src/app/components/recently-added/recently-added.component.ts
+++ src/app/components/recently-added/recently-added.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LibraryService } from 'src/app/services/library.service';
+import { Subscription } from 'rxjs';
+import { MusicKitService } from 'src/app/services/music-kit.service';
+
+const artworkWidth = 180;
+const artworkHeight = 180;
 
 @Component({
   selector: 'app-recently-added',
   templateUrl: './recently-added.component.html',
   styleUrls: ['./recently-added.component.css']
 })
-export class RecentlyAddedComponent implements OnInit {
+export class RecentlyAddedComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private recentlyAddedAlbums: any[] = []; //TODO: these are being destroyed and retrieved again every time the user changes routes. Need to keep this somehow
+  private subscriptions: Subscription = new Subscription();
 
-  ngOnInit() {
+  constructor(private libraryService: LibraryService, private musicKitService: MusicKitService) { }
+
+  ngOnInit(): void {
+    this.getRecentlyAdded(0);
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  getRecentlyAdded(startIndex: number): void {
+    this.subscriptions.add( 
+      this.libraryService.getRecentlyAdded(startIndex).subscribe( recentlyAddedAlbums => {
+        if(recentlyAddedAlbums.length) {
+          this.recentlyAddedAlbums = this.recentlyAddedAlbums.concat(recentlyAddedAlbums);
+
+          //TODO: remove the length check? or should recently added be limited? will apple even let me get the entire history?
+          if(this.recentlyAddedAlbums.length < 100) {
+            this.getRecentlyAdded(startIndex + this.libraryService.getCollectionRequestLimit());
+          }
+        }
+      })
+    );
+  }
+
+  getValidArtworkUrl(artworkUrl: string): string {
+    return this.musicKitService.getFormattedArtworkUrl(artworkUrl, artworkWidth, artworkHeight);
+  }
+
+  getArtworkWidth(): number {
+    return artworkWidth;
+  }
+
+  getArtworkHeight(): number {
+    return artworkHeight;
+  }
 }
--- src/app/services/library.service.ts
+++ src/app/services/library.service.ts
@@ -7,15 +7,25 @@ import { MusicKitService } from './music-kit.service';
 })
 export class LibraryService {
   private songRequestLimit: number = 100;
+  private collectionRequestLimit: number = 10;
   private library: any = this.musicKitService.getLibrary();
 
   constructor(private musicKitService: MusicKitService) { }
 
   getSongs(startIndex: number): Observable<any> {
-    return from(this.library.songs( null , { limit: this.songRequestLimit, offset: startIndex }));
+    return from(this.library.songs(null , { limit: this.songRequestLimit, offset: startIndex }));
+  }
+
+  getRecentlyAdded(startIndex: number): Observable<any> {
+    return from(this.library.collection(
+      'recently-added', null, { limit: this.collectionRequestLimit, offset: startIndex }));
   }
 
   getSongRequestLimit(): number {
     return this.songRequestLimit;
+  }
+
+  getCollectionRequestLimit(): number {
+    return this.collectionRequestLimit;
   }
 }
