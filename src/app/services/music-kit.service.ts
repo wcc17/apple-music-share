e04043fa5e8af850714201a8708555c0e00d24aa
William Curry <christian.curry17@gmail.com>
2020-12-14 14:53:27 -0500

    fix

---
+++ src/app/services/music-kit.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+
+declare var MusicKit: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MusicKitService {
+  private collectionRequestLimit: number = 10;
+  private libraryRequestLimit: number = 100;
+  private isAuthorized: boolean = false;
+  private musicKit: any;
+
+  constructor() { 
+    MusicKit.configure({
+      developerToken: 'test',
+      app: { 
+        name: 'Apple Music Share',
+        build: '1.0'
+      }
+    });
+
+    //ensure user is authorized
+    this.musicKit = MusicKit.getInstance();
+    this.isAuthorized = this.musicKit.isAuthorized;
+  }
+
+  authorizeUser(): void {
+    if(!this.isAuthorized) {
+      from(this.musicKit.authorize()).subscribe(() => {
+        this.isAuthorized = true;
+      })
+    }
+  }
+
+  unauthorizeUser(): void {
+    from(this.musicKit.unauthorize()).subscribe(() => {
+      this.isAuthorized = false;
+    })
+  }
+
+  isUserAuthorized(): boolean {
+    return this.isAuthorized;
+  }
+
+  setQueue(arg: any): Observable<any> {
+    return this.musicKit.setQueue(arg);
+  }
+
+  getPlayer(): any {
+    return this.musicKit.player;
+  }
+
+  getLibrary(): any {
+    return this.musicKit.api.library;
+  }
+
+  getApi(): any {
+    return this.musicKit.api;
+  }
+
+  getFormattedArtworkUrl(artworkUrl: string, width: number, height: number): string {
+    return MusicKit.formatArtworkURL( { 'url': artworkUrl }, height, width );
+  }
+
+  getFormattedMilliseconds(milliseconds: number) {
+    return MusicKit.formattedMilliseconds(milliseconds);
+  }
+
+  addMediaItemDidChangeEventListener(eventListener: any) {
+    this.musicKit.addEventListener( MusicKit.Events.mediaItemDidChange, eventListener );
+  }
+
+  addPlaybackDidChangeEventListener(eventListener: any) {
+    this.musicKit.addEventListener( MusicKit.Events.playbackStateDidChange, eventListener );
+  }
+
+  getDeveloperToken() {
+    return this.musicKit.developerToken;
+  }
+
+  getMusicUserToken() {
+    return this.musicKit.musicUserToken;
+  }
+
+  getLibraryRequestLimit(): number {
+    return this.libraryRequestLimit;
+  }
+
+  getCollectionRequestLimit(): number {
+    return this.collectionRequestLimit;
+  }
+}
